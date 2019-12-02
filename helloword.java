b40da0a0add5f6e2b29e2c073af7e09a19ec75b7
=ZeroJoy <ttz1392770709@163.com>
2019-12-02 13:59:19 +0800

    hello world

---
+++ helloword.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("H11111");
+		System.out.println("He22222");
+		System.out.println("Hel3333");
+		System.out.println("He54444d4");
+		System.out.println("He55555");
+		System.out.println("He666rld6");
+    }
+}
